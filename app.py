04c98703940205a16f60a20148bead4565a5040c
SAJID KHAN <suzankhan06@gmail.com>
2023-08-02 14:04:01 +0300

    changes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+# from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup
 #from urllib.request import urlopen as uReq
 import logging
-import pymongo
+# import pymongo
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 import os
 
